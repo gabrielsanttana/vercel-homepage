d7f215104014c00fbd38c146d6e21c038e7d0405
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-02-08 23:25:26 -0300

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
-import './App.module.scss';
+import styles from './App.module.scss';
+import Logo from './assets/logo.svg';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Vercel Homepage</h1>
+    <div className={styles.container}>
+      <header>
+        <figure className={styles.left}>
+          <img src={Logo} alt="Vercel Logo" />
+        </figure>
+
+        <nav className={styles.middle}>
+          <ul>
+            <li>
+              <a href="#">Solutions</a>
+            </li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.right}>
+          <button>Next.js Conf</button>
+
+          <a href="#">Login</a>
+
+          <button>Sign Up</button>
+        </div>
+      </header>
+
+      <main>
+        <h1>
+          <span>Develop.</span>
+          <span>Preview.</span>
+          <span>Ship.</span>
+        </h1>
+
+        <div className={styles.buttonsContainer}>
+          <button>Start coding</button>
+
+          <button className={styles.outline}>Get a demo</button>
+        </div>
+
+        <h2>
+          Vercel combines the best developer expirience with an obsessive focus
+          on end-user performance.
+          <br />
+          Our platform enables frontend teams to do their best work.
+        </h2>
+      </main>
+
+      <footer>Explore the Vercel way</footer>
     </div>
   );
 };
